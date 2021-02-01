734dc931f2433940987b58dadb2f47f7122f62b6
bwuphan <bwuphan@gmail.com>
2021-01-31 18:21:58 -0800

    Add more tests

--- indexSpec.js
+++ indexSpec.js
@@ -1,5 +1,11 @@
 describe('caesarCipher test', () => {
-  const cipher = require('./index.js')
+  const cipher = require('./index.js').cipher
+
+  it('returns an empty string if no string is present.', () => {
+    expect(cipher('')).toEqual('');
+    expect(cipher('', 2)).toEqual('');
+  })
+
   it('returns same string if no shift is present.', () => {
     expect(cipher('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG')).toEqual('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG');
   });
@@ -9,10 +15,28 @@ describe('caesarCipher test', () => {
   });
 
   it('shifts the string right if a positive number is passed in.', () => {
-    expect(cipher('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG', 3)).toEqual('QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD');
+    expect(cipher('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG', 3)).toEqual('WKH TXLFN EURZQ IRA MXPSV RYHU WKH ODCB GRJ');
   });
 
   it('shifts the string left if negative number is passed in.', () => {
-    expect(cipher('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG', -10)).toEqual('DRO AESMU LBYGX PYH TEWZC YFOB DRO VKJI NYQ');
+    expect(cipher('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG', -3)).toEqual('QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD');
   });  
+
+  it('shifts correctly even if shift is greater than number of letters in alphabet', () => {
+    expect(cipher('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG', 27)).toEqual('UIF RVJDL CSPXO GPY KVNQT PWFS UIF MBAZ EPH');
+  });
+
+  it('shifts lower case letters.', () => {
+    expect(cipher('the quick brown fox jumps over the lazy dog', 3)).toEqual('wkh txlfn eurzq ira mxpsv ryhu wkh odcb grj');
+  });
+
+  it('shifts a mix of lower case and upper case letters.', () => {
+    expect(cipher('tHe quick BROWN fox jUmPs over the LazY Dog', 3)).toEqual('wKh txlfn EURZQ ira mXpSv ryhu wkh OdcB Grj');
+  });  
+
+  it('returns an empty string if one character is not a letter or whitespace.', () => {
+    expect(cipher('TH-E QUICK BROWN FOX JUMPS OVER THE LAZY DOG', 3)).toEqual('');
+    expect(cipher('2', 3)).toEqual('');
+    expect(cipher('er@', 3)).toEqual('');
+  });    
 });
