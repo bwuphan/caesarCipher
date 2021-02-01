7c8e3d778b8205210156148a3185264d99583a87
bwuphan <bwuphan@gmail.com>
2021-01-31 17:23:08 -0800

    Add some specs

--- indexSpec.js
+++ indexSpec.js
@@ -1,6 +1,14 @@
-describe('addTwoNumbers', () => {
-
-  it('passes test case 1.', () => {
-    expect(true).toEqual(true);
+describe('caesarCipher test', () => {
+  const cipher = require('./index.js')
+  it('returns same string if no shift is present.', () => {
+    expect(cipher('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG')).toEqual('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG');
   });
+
+  it('shifts the string right if a positive number is passed in.', () => {
+    expect(cipher('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG')).toEqual('QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD');
+  });
+
+  it('shifts the string right if a positive number is passed in.', () => {
+    expect(cipher('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG')).toEqual('WKH TXLFN EURZQ IRA MXPSV RYHU WKH ODCB GRJ');
+  });  
 });
